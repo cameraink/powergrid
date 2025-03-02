225ee1421263b96f4dad149038b831048ba0815b
Guillaume Dion <guillaume.dion@qiti.eu>
2025-03-03 00:33:07 +0100

    Add negative examples

--- examples/example_finder.py
+++ examples/example_finder.py
@@ -6,14 +6,19 @@ finder = TsoFinder()
 print(f'\n- The TSO for the region code ‘FR-IDF‘ is {finder.by_region("FR-IDF")}')  # Output: "TSO_FR_001"
 
 # search by_region is case-insensitive
-print(f'- The TSO for the region code ‘fr-idf‘ is {finder.by_region("fr-idf")}')  # Output: "TSO_FR_001"
+print(f'- The TSO for the region code ‘fr-idf‘ is: {finder.by_region("fr-idf")}')  # Output: "TSO_FR_001"
 
 # Unknown region code
-print(f'\n- The TSO for the region code ‘ab-cd‘ is {finder.by_region("ab-cd")}')  # Output: None
+print(f'\n- The TSO for the region code ‘ab-cd‘ is: {finder.by_region("ab-cd")}')  # Output: None
 
 # Get TSO details
-print(f'\n- The region codes for the TSO ID ‘TSO_FR_001‘ is {finder.by_tsoid("TSO_FR_001")}')  #  Output: List of regions
+print(f'\n- The region codes for the TSO ID ‘TSO_FR_001‘ are: {finder.by_tsoid("TSO_FR_001")}')  #  Output: List of regions
+
+# check unknown TSO
+print(f'- The region codes for the TSO ID ‘ABCD‘ are: {finder.by_tsoid("ABCD")}')  #  Output: None
 
 # Find TSO by ENTSO-E code
-print(f'\n- The Tso ID for the ENTSOE code ‘10YFR-RTE------C‘ is {finder.by_entsoe("10YFR-RTE------C")}')   # Output: Tso object for RTE
+print(f'\n- The Tso ID for the ENTSOE code ‘10YFR-RTE------C‘ is: {finder.by_entsoe("10YFR-RTE------C")}')   # Output: Tso object for RTE
 
+# check unknown ENTSOE
+print(f'- The Tso ID for the ENTSOE code ‘ABCD‘ is: {finder.by_entsoe("ABCD")}')   # Output: None
